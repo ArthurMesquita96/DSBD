5b85b4021372ee83032ae5b224813d2c038b4911
ArthurMesquita96 <arthur.mesquita@engenharia.ufjf.br>
2024-05-25 11:41:02 -0300

    multiprocessing condicao de corrida

--- infraestrutura-computacional/multiprocessing/pi_naive_pipes.py
+++ infraestrutura-computacional/multiprocessing/pi_naive_pipes.py
@@ -1,16 +1,18 @@
 import time
-from multiprocessing import Process
+from multiprocessing import Process, Pipe, Lock
 
-
-def pi_naive(start, end, step):
-    print ("Start: ", str(start))
-    print ("End: ", str(end))
-    print ("Step: ", str(step))
+def pi_naive(start, end, step, a, lock):
+    # print ("Start: ", str(start))
+    # print ("End: ", str(end))
     sum = 0.0
 
     for i in range(start, end):
         x = (i+0.5) * step
         sum = sum + 4.0/(1.0+x*x)
+
+    with lock:
+        a.send(sum) ## condição de corrida
+
     return print(sum * step)
 
 if __name__ == "__main__":
@@ -20,27 +22,35 @@ if __name__ == "__main__":
     n_workers = 4
     loop_range = num_steps/n_workers
 
+    lock = Lock()
+
+    a, b = Pipe()
+
     procs = []
     for i in range(n_workers):
         inicio = int(i * loop_range)
         fim = int((i + 1) * loop_range)
-        print(inicio)
-        print(fim)
-        print(step)
+        # print(inicio)
+        # print(fim)
+        # print(step)
 
-        p = Process(target=pi_naive, args=(inicio, fim, step,))
+        p = Process(target=pi_naive, args=(inicio, fim, step,a, lock, ))
         procs.append(p)
 
     tic = time.time() # Tempo Inicial
-    # sums = pi_naive(0, num_steps, step)
     for i in range(n_workers):
         procs[i].start()
     
+    for i in range(n_workers):
+        sum = b.recv()
+        sums = sums+sum
+
     for i in range(n_workers):
         procs[1].join()
 
     toc = time.time() # Tempo Final
 
-    # pi = step * sums
-    # print ("Valor Pi: %.10f" %pi)
+
+    pi = step * sums
+    print ("Valor Pi: %.10f" %pi)
     # print ("Tempo Pi: %.8f s" %(toc-tic))
--- infraestrutura-computacional/multiprocessing/pi_naive_queue.py
+++ infraestrutura-computacional/multiprocessing/pi_naive_queue.py
@@ -1,16 +1,17 @@
 import time
-from multiprocessing import Process
+from multiprocessing import Process, Pipe, Queue
 
-
-def pi_naive(start, end, step):
-    print ("Start: ", str(start))
-    print ("End: ", str(end))
-    print ("Step: ", str(step))
+def pi_naive(start, end, step, fila):
+    # print ("Start: ", str(start))
+    # print ("End: ", str(end))
     sum = 0.0
 
     for i in range(start, end):
         x = (i+0.5) * step
         sum = sum + 4.0/(1.0+x*x)
+
+    fila.put(sum)
+
     return print(sum * step)
 
 if __name__ == "__main__":
@@ -20,27 +21,33 @@ if __name__ == "__main__":
     n_workers = 4
     loop_range = num_steps/n_workers
 
+    fila = Queue()
+
     procs = []
     for i in range(n_workers):
         inicio = int(i * loop_range)
         fim = int((i + 1) * loop_range)
-        print(inicio)
-        print(fim)
-        print(step)
+        # print(inicio)
+        # print(fim)
+        # print(step)
 
-        p = Process(target=pi_naive, args=(inicio, fim, step,))
+        p = Process(target=pi_naive, args=(inicio, fim, step, fila, ))
         procs.append(p)
 
     tic = time.time() # Tempo Inicial
-    # sums = pi_naive(0, num_steps, step)
     for i in range(n_workers):
         procs[i].start()
     
+    for i in range(n_workers):
+        sum = fila.get()
+        sums = sums+sum
+
     for i in range(n_workers):
         procs[1].join()
 
     toc = time.time() # Tempo Final
 
-    # pi = step * sums
-    # print ("Valor Pi: %.10f" %pi)
+
+    pi = step * sums
+    print ("Valor Pi: %.60f" %pi)
     # print ("Tempo Pi: %.8f s" %(toc-tic))
